6427ae4bfa6c4fd53e793756a940807b36d928af
David Kalandadze <therapsody4@gmail.com>
2023-04-29 04:29:28 +0400

    Finish

---
+++ Work.js
@@ -0,0 +1,78 @@
+// Arithmetic Operators on numbers
+
+let x = 5; 
+
+console.log ( x + 2 ); // 7
+console.log ( x - 6 ); // -1
+console.log ( x * 30); // 150
+console.log ( x / 2 ); // 2.5
+console.log ( x % 5 ); // 0
+console.log ( x ** 3); // 125
+
+/* Assignment Operator */
+
+let p = 5;
+console.log(p);
+console.log (p = p + 5); //10
+console.log (p -=6); //4
+console.log (p *=30); //120
+console.log (p /=2); //60
+console.log (p %=5); //0
+console.log (p **=3); //0
+
+console.log(p);
+
+/* Comparison Operators */
+
+let f = 0.1;
+let n = 0.2;
+
+console.log (f + n);
+
+let a = true;
+let b = "true";
+
+console.log (a === b); //false
+
+let c = 1;
+let d = 7;
+
+console.log (c + d != 8); //false
+
+let e = 10**5;
+let g = 9**6;
+
+console.log(e > g);
+
+let k = 17/6;
+let m = 4;
+
+console.log(k <= m); //true
+
+/* Conditionals */
+
+let firstName = "Neo";
+let age = 3;
+let price = 35;
+if (age < 18){
+    console.log('Underage, Price is free')
+} 
+
+
+
+else if (firstName = "James");
+else if (age = 25); {
+    console.log('discount by 20%')
+}
+
+/* Arrays */
+
+
+
+
+
+
+
+
+
+
